9a55d2ee24e15fca430cea90ca06290495a64a46
Daniel Heredia <dheredia@kionetworks.com>
2021-09-17 14:24:43 -0500

    test

--- src/app/services/test.service.ts
+++ src/app/services/test.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from 'src/environments/environment';
+
 export default class ResponseModel {
   statusCode?: number;
   headers?: any;
@@ -13,12 +15,12 @@ export default class ResponseModel {
 })
 export class TestService {
 
-  // SERVICE_URL: string = 'https://vu3cpov8r3.execute-api.us-east-2.amazonaws.com/desarrollo';
   SERVICE_URL: string = 'https://yacop2d062.execute-api.us-east-2.amazonaws.com/Stage/api/categories';
 
   constructor(private httpClient: HttpClient) { }
 
   greet(): Observable<ResponseModel> {
+    console.log(environment.apiUrl);
     return this.httpClient.get<ResponseModel>(this.SERVICE_URL);
   }
 
